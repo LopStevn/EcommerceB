a9cd14fe84409df01694831cadaa5951a079c119
LopStevn <stevenlpz03@gmail.com>
2024-04-10 23:03:14 -0600

    Se agregaron los servicios de Categoria

---
+++ EcommerceB.Servicio/Contrato/ICategoriaServicio.cs
@@ -0,0 +1,22 @@
+﻿using EcommerceB.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EcommerceB.Servicio.Contrato
+{
+    public interface ICategoriaServicio
+    {
+        Task<List<CategoriaDTO>> Lista(string buscar);
+
+        Task<CategoriaDTO> Obtener(CategoriaDTO modelo);
+
+        Task<CategoriaDTO> Crear(CategoriaDTO modelo);
+
+        Task<bool> Editar(CategoriaDTO modelo);
+
+        Task<bool> Eliminar(int id);
+    }
+}
