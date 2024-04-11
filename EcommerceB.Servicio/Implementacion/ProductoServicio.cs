f50bcfdf2cb03ecb48e5b0080e5b136edfe4ce82
LopStevn <stevenlpz03@gmail.com>
2024-04-11 10:55:10 -0600

    Se agregaron los servicios de Venta y Producto

---
+++ EcommerceB.Servicio/Implementacion/ProductoServicio.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using EcommerceB.Modelo;
+using EcommerceB.DTO;
+using EcommerceB.Repositorio.Contrato;
+using EcommerceB.Servicio.Contrato;
+using AutoMapper;
+
+namespace EcommerceB.Servicio.Implementacion
+{
+    public class ProductoServicio : IProductoServicio
+    {
+        private readonly IGenericoRepositorio<Producto> _modeloRepositorio;
+        private readonly IMapper _mapper;
+
+        public ProductoServicio(IGenericoRepositorio<Producto> modeloRepositorio, IMapper mapper)
+        {
+            _modeloRepositorio = modeloRepositorio;
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductoDTO>> Catalogo(string categoria, string buscar)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consulta(p =>
+                p.Nombre.ToLower().Contains(buscar.ToLower()) &&
+                p.IdCategoriaNavigation.Nombre.ToLower().Contains(categoria.ToLower()));
+
+                List<ProductoDTO> lista = _mapper.Map<List<ProductoDTO>>(await consulta.ToListAsync());
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<ProductoDTO> Crear(ProductoDTO modelo)
+        {
+            try
+            {
+                var dbModelo = _mapper.Map<Producto>(modelo);
+                var rspModelo = await _modeloRepositorio.Crear(dbModelo);
+
+                if (rspModelo.IdCategoria != 0)
+                    return _mapper.Map<ProductoDTO>(rspModelo);
+                else
+                    throw new TaskCanceledException("No se pudo crear");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool> Editar(ProductoDTO modelo)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consulta(p => p.IdProducto == modelo.IdProducto);
+                var fromDbModelo = await consulta.FirstOrDefaultAsync();
+
+                if (fromDbModelo != null)
+                {
+                    fromDbModelo.Nombre = modelo.Nombre;
+                    fromDbModelo.Descripcion = modelo.Descripcion;
+                    fromDbModelo.Cantidad = modelo.Cantidad;
+                    fromDbModelo.Precio = modelo.Precio;
+                    fromDbModelo.PrecioOferta = modelo.PrecioOferta;
+                    fromDbModelo.Imagen = modelo.Imagen;
+                    fromDbModelo.IdCategoria = modelo.IdCategoria;
+
+                    var respuesta = await _modeloRepositorio.Editar(fromDbModelo);
+
+                    if (!respuesta)
+                        throw new TaskCanceledException("No se pudo editar");
+
+                    return respuesta;
+                }
+                else
+                {
+                    throw new TaskCanceledException("No se encontraron resultados");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<bool> Eliminar(int id)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consulta(p => p.IdProducto == id);
+                var fromDbModelo = await consulta.FirstOrDefaultAsync();
+
+                if (fromDbModelo != null)
+                {
+                    var respuesta = await _modeloRepositorio.Eliminar(fromDbModelo);
+
+                    if (!respuesta)
+                        throw new TaskCanceledException("No se pudo eliminar");
+
+                    return respuesta;
+                }
+                else
+                {
+                    throw new TaskCanceledException("No se encontraron resultados");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<List<ProductoDTO>> Lista(string buscar)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consulta(p =>
+                p.Nombre.ToLower().Contains(buscar.ToLower()));
+
+                consulta = consulta.Include(c => c.IdCategoriaNavigation);
+
+                List<ProductoDTO> lista = _mapper.Map<List<ProductoDTO>>(await consulta.ToListAsync());
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public async Task<ProductoDTO> Obtener(ProductoDTO modelo)
+        {
+            try
+            {
+                var consulta = _modeloRepositorio.Consulta(p => p.IdProducto == modelo.IdProducto);
+                consulta = consulta.Include(c => c.IdCategoriaNavigation);
+                var fromDbModelo = await consulta.FirstOrDefaultAsync();
+
+                if (fromDbModelo != null)
+                    return _mapper.Map<ProductoDTO>(fromDbModelo);
+                else
+                    throw new TaskCanceledException("No se encontraron coincidencias");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
