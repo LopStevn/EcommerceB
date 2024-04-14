58474fb681dee8eaf26c006a21cbd163e3a874d6
LopStevn <stevenlpz03@gmail.com>
2024-04-14 17:40:51 -0600

    Implementacion de Servicios en WebAssembly - LocalStorage y Toast de Blazored

--- EcommerceB.WebAssembly/Program.cs
+++ EcommerceB.WebAssembly/Program.cs
@@ -2,10 +2,26 @@ using EcommerceB.WebAssembly;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
+using Blazored.LocalStorage;
+using Blazored.Toast;
+
+using EcommerceB.WebAssembly.Servicio.Contrato;
+using EcommerceB.WebAssembly.Servicio.Implementacion;
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5231/api/") });
+
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddBlazoredToast();
+
+builder.Services.AddScoped<IUsuarioServicio, UsuarioServicio>();
+builder.Services.AddScoped<ICategoriaServicio, CategoriaServicio>();
+builder.Services.AddScoped<IProductoServicio, ProductoServicio>();
+builder.Services.AddScoped<ICarritoServicio, CarritoServicio>();
+builder.Services.AddScoped<IVentaServicio, VentaServicio>();
+builder.Services.AddScoped<IDashboardServicio, DashboardServicio>();
 
 await builder.Build().RunAsync();
